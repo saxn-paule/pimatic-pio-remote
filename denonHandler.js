28f9dbd3c75a93453f2db8a604a268a407765e31
Paul Schmidt <paul.schmidt@b-s-s.de>
2016-01-04 07:48:35 +0100

    Fixed typo

--- denonHandler.js
+++ denonHandler.js
@@ -1,6 +1,6 @@
 module.exports.handleData = function(stringyfiedData, env, cb) {          
   if(stringyfiedData.indexOf('MV') === 0) {
-	var currentVol = stringyfiedData.substring(3,stringifiedData.length -1) - 80;
+	var currentVol = stringyfiedData.substring(3,stringyfiedData.length -1) - 80;
   } else {
 	env.logger.info('Received: ' + stringyfiedData);
   }
